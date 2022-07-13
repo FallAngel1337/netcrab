2a0723b24f3aaf22231a14d48ff513458438ef4e
FallAngel1337 <fallangel@protonmail.com>
2022-07-13 13:55:13 -0300

    Add the argument parser module

---
+++ src/args.rs
@@ -0,0 +1,48 @@
+use std::net::SocketAddr;
+
+#[macro_export]
+macro_rules! help {
+    () => {
+        {
+            print!("{HELP_MENU}");
+            std::process::exit(0);
+        }
+    };
+}
+
+const HELP_MENU: &str = "rustcat v0.1.0
+Usage: rustcat [OPTION] [<addr:port>]
+Options:
+-h,  --help           Show this message
+-l,  --listen         Listen mode\n";
+
+
+#[derive(Debug, Clone, Copy)]
+pub struct NetCrabArgs {
+    addr: Option<SocketAddr>,
+    listen: bool
+}
+
+impl NetCrabArgs {
+    pub fn new() -> Self {
+        let args = std::env::args().skip(1).take(2);
+        if args.len() == 0 {
+            help!()
+        }
+        
+        let mut res = Self {
+            addr: None,
+            listen: false,
+        };
+
+        for arg in args {
+            match arg.as_ref() {
+                "-h" | "--help" => help!(),
+                "-l" | "--listen" => res.listen = true,
+                _ => res.addr = Some(arg.parse().unwrap()),
+            }
+        }
+
+        res
+    }
+}
